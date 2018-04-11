dfd2e54320e274b19bbe52c97a0531319d5918f1
AlexanderJChoi <alexanderajchoi@gmail.com>
2018-04-11 11:43:16 -0700

    Properly define NULL if undefined

--- types.h
+++ types.h
@@ -2,4 +2,6 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
-#define NULL ( (void *) 0)
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
