714ab42412fac331591579060489edef2c47e7ce
HSanka6 <haasith2@gmail.com>
2018-04-13 11:58:56 -0700

    Added waitpid function

--- proc.c
+++ proc.c
@@ -318,13 +318,42 @@ int
 waitpid(int pid, int* status, int options)
 {
   struct proc *p;
+  int havekids, pid;
   struct proc *curproc = myproc();
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
-    if(p->parent->pid  == pid)
-      return pid
-  }
-  if(curproc->killed) {
-    return -1;
+  if(status != NULL)
+    status = &curproc->exit_status; 
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE && p->id == pid){
+        // Found one.
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
 }
 
