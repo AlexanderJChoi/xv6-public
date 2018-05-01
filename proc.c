8ed57da6202fa4e125c2fdb4b334146cabe4862c
AlexanderJChoi <alexanderajchoi@gmail.com>
2018-05-01 11:29:52 -0700

    Add Priority Scheduling to Scheduler

--- proc.c
+++ proc.c
@@ -379,6 +379,7 @@ void
 scheduler(void)
 {
   struct proc *p;
+  struct proc *maxPriority;
   struct cpu *c = mycpu();
   c->proc = 0;
   
@@ -388,24 +389,26 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+    for(p = ptable.proc, maxPriority = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state == RUNNABLE && p->priority > maxPriority->priority) {
+         maxPriority = p;
+      }
+    }
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+      c->proc = maxPriority;
+      switchuvm(maxPriority);
+      maxPriority->state = RUNNING;
 
-      swtch(&(c->scheduler), p->context);
+      swtch(&(c->scheduler), maxPriority->context);
       switchkvm();
 
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
-    }
+
     release(&ptable.lock);
 
   }
